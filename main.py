35b7b3b730fd449933777b3647b1237478da81db
Qds11 <limqiqing96@gmail.com>
2023-11-27 15:27:31 +0900

    start screen

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ pygame.init()
 # Set up display
 window_size = (900, 700)
 screen = pygame.display.set_mode(window_size)
-# pygame.display.set_caption("player Movement")
+pygame.display.set_caption("Space Explorer")
 
 # constants
 HEALTH_DECREASE_RATE = 0.05
@@ -60,8 +60,9 @@ direction = "right"
 # List to store treat positions
 treat_positions = []
 
-# Load font for displaying points
+# Fonts
 font = pygame.font.Font(None, 36)
+title_font = pygame.font.Font(None, 72)
 
 points = 0
 
@@ -116,6 +117,61 @@ asteroid_timers = []
 # Create a sprite group for asteroids
 asteroids_group = pygame.sprite.Group()
 
+def start_screen():
+    screen.blit(background, (0, 0))
+
+    title_text = title_font.render("Space Explorer", True, WHITE)
+    title_rect = title_text.get_rect(center=(window_size[0] // 2, window_size[1] // 4))
+    screen.blit(title_text, title_rect)
+
+    start_button = pygame.Rect(window_size[0] // 4, window_size[1] // 2, window_size[0] // 2, 50)
+    pygame.draw.rect(screen, (0, 255, 0), start_button)
+    start_text = font.render("Start Game", True, WHITE)
+    start_text_rect = start_text.get_rect(center=start_button.center)
+    screen.blit(start_text, start_text_rect)
+
+    instructions_button = pygame.Rect(window_size[0] // 4, window_size[1] // 2 + 100, window_size[0] // 2, 50)
+    pygame.draw.rect(screen, (0, 255, 0), instructions_button)
+    instructions_text = font.render("Instructions", True, WHITE)
+    instructions_text_rect = instructions_text.get_rect(center=instructions_button.center)
+    screen.blit(instructions_text, instructions_text_rect)
+
+    pygame.display.flip()
+
+    return start_button, instructions_button
+
+# Function to display instructions
+def display_instructions():
+    screen.blit(background, (0, 0))
+
+    title_text = title_font.render("Instructions", True, WHITE)
+    title_rect = title_text.get_rect(center=(window_size[0] // 2, 50))
+    screen.blit(title_text, title_rect)
+
+    instruction_texts = [
+        "1. Collect oxygen tanks to supplement your depleting oxygen",
+        "2. Avoid asteroids",
+        "3. Survive as long as possible",
+    ]
+
+    for i, instruction in enumerate(instruction_texts):
+        text = font.render(instruction, True, WHITE)
+        text_rect = text.get_rect(center=(window_size[0] // 2, 150 + i * 50))
+        screen.blit(text, text_rect)
+
+    back_button = pygame.Rect(window_size[0] // 4, window_size[1] - 100, window_size[0] // 2, 50)
+    pygame.draw.rect(screen, (0, 255, 0), back_button)
+    back_text = font.render("Back to Menu", True, WHITE)
+    back_text_rect = back_text.get_rect(center=back_button.center)
+    screen.blit(back_text, back_text_rect)
+
+    pygame.display.flip()
+
+    return back_button
+
+# Initial state
+current_state = "start_screen"
+
 # Main game loop
 clock = pygame.time.Clock()
 spawn_timer = 0
@@ -132,145 +188,163 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if current_state == "start_screen":
+                start_button, instructions_button = start_screen()
+                if start_button.collidepoint(event.pos):
+                    current_state = "playing"
+                    start_time = pygame.time.get_ticks()
+                elif instructions_button.collidepoint(event.pos):
+                    current_state = "instructions"
+            elif current_state == "instructions":
+                back_button = display_instructions()
+                if back_button.collidepoint(event.pos):
+                    current_state = "start_screen"
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        player_rect.x -= speed
-        direction = "left"
-        # Ensure player stays within the left boundary
-        player_rect.x = max(player_rect.x, 0)
-    elif keys[pygame.K_RIGHT]:
-        player_rect.x += speed
-        direction = "right"
-        # Ensure player stays within the right boundary
-        player_rect.x = min(player_rect.x, window_size[0] - player_rect.width)
-    elif keys[pygame.K_UP]:
-        player_rect.y -= speed
-        # Ensure player stays within the top boundary
-        player_rect.y = max(player_rect.y, 0)
-    elif keys[pygame.K_DOWN]:
-        player_rect.y += speed
-        # Ensure player stays within the bottom boundary
-        player_rect.y = min(player_rect.y, window_size[1] - player_rect.height)
+    if current_state == "start_screen":
+        start_screen()
+    elif current_state == "instructions":
+        display_instructions()
+    elif current_state == "playing":
 
-    # Update the smaller collision rect's position
-    player_collision_rect.topleft = (player_rect.x + 40, player_rect.y + 20)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player_rect.x -= speed
+            direction = "left"
+            # Ensure player stays within the left boundary
+            player_rect.x = max(player_rect.x, 0)
+        elif keys[pygame.K_RIGHT]:
+            player_rect.x += speed
+            direction = "right"
+            # Ensure player stays within the right boundary
+            player_rect.x = min(player_rect.x, window_size[0] - player_rect.width)
+        elif keys[pygame.K_UP]:
+            player_rect.y -= speed
+            # Ensure player stays within the top boundary
+            player_rect.y = max(player_rect.y, 0)
+        elif keys[pygame.K_DOWN]:
+            player_rect.y += speed
+            # Ensure player stays within the bottom boundary
+            player_rect.y = min(player_rect.y, window_size[1] - player_rect.height)
 
-    # Randomly spawn oxygen_tank
-    spawn_timer += clock.get_rawtime()
-    if spawn_timer >= spawn_interval:
-        spawn_timer = 0
-        treat_x = random.randint(0, window_size[0] - oxygen_tank_image.get_width())
-        treat_y = random.randint(0, window_size[1] - oxygen_tank_image.get_height())
-        treat_rect = pygame.Rect(treat_x, treat_y, oxygen_tank_image.get_width(), oxygen_tank_image.get_height())
+        # Update the smaller collision rect's position
+        player_collision_rect.topleft = (player_rect.x + 40, player_rect.y + 20)
 
-        # Check if player is not at the treat location and there are no other oxygen_tank there
-        if (
-            not player_rect.colliderect(treat_rect)
-            and not any(treat_rect.colliderect(existing_treat) for existing_treat in treat_positions)
-        ):
-            treat_positions.append(treat_rect)
-            # Add a timer for the treat (5 to 7 seconds)
-            treat_timers.append(random.randint(300, 700))
+        # Randomly spawn oxygen_tank
+        spawn_timer += clock.get_rawtime()
+        if spawn_timer >= spawn_interval:
+            spawn_timer = 0
+            treat_x = random.randint(0, window_size[0] - oxygen_tank_image.get_width())
+            treat_y = random.randint(0, window_size[1] - oxygen_tank_image.get_height())
+            treat_rect = pygame.Rect(treat_x, treat_y, oxygen_tank_image.get_width(), oxygen_tank_image.get_height())
 
-    # Draw background and player
-    screen.blit(background, (0, 0))
+            # Check if player is not at the treat location and there are no other oxygen_tank there
+            if (
+                not player_rect.colliderect(treat_rect)
+                and not any(treat_rect.colliderect(existing_treat) for existing_treat in treat_positions)
+            ):
+                treat_positions.append(treat_rect)
+                # Add a timer for the treat (5 to 7 seconds)
+                treat_timers.append(random.randint(300, 700))
 
-    # Draw oxygen_tank and update timers
-    for i in range(len(treat_positions)):
-        screen.blit(oxygen_tank_image, treat_positions[i])
-        treat_timers[i] -= clock.get_rawtime()
-        if player_collision_rect.colliderect(treat_positions[i]):
-            # Increment points and remove oxygen_tank
-            health += 0.1
-            treat_positions.pop(i)
-            treat_timers.pop(i)
-            break
-        if treat_timers[i] <= 0:
-            # Remove oxygen_tank and its timer when the time is up
-            treat_positions.pop(i)
-            treat_timers.pop(i)
-            break
+        # Draw background and player
+        screen.blit(background, (0, 0))
 
-    # Spawn asteroid
-    spawn_timer_asteroid += clock.get_rawtime()
-    if spawn_timer_asteroid >= spawn_interval_asteroid:
-        spawn_timer_asteroid = 0
-        spawn_asteroid()
+        # Draw oxygen_tank and update timers
+        for i in range(len(treat_positions)):
+            screen.blit(oxygen_tank_image, treat_positions[i])
+            treat_timers[i] -= clock.get_rawtime()
+            if player_collision_rect.colliderect(treat_positions[i]):
+                # Increment points and remove oxygen_tank
+                health += 0.1
+                treat_positions.pop(i)
+                treat_timers.pop(i)
+                break
+            if treat_timers[i] <= 0:
+                # Remove oxygen_tank and its timer when the time is up
+                treat_positions.pop(i)
+                treat_timers.pop(i)
+                break
 
-    # Move asteroids and update timers
-    for i in range(len(asteroid_positions)):
-        asteroid_positions[i].x += asteroid_speed * asteroid_velocities[i][0]  # Adjust the speed as needed
-        asteroid_positions[i].y += asteroid_speed * asteroid_velocities[i][1]
-        screen.blit(asteroid_image, asteroid_positions[i])
-        asteroid_timers[i] -= clock.get_rawtime()
-        if asteroid_timers[i] <= 0:
-            # Remove asteroid, its timer, and velocity when the time is up
-            asteroid_positions.pop(i)
-            asteroid_velocities.pop(i)
-            asteroid_timers.pop(i)
-            break
+        # Spawn asteroid
+        spawn_timer_asteroid += clock.get_rawtime()
+        if spawn_timer_asteroid >= spawn_interval_asteroid:
+            spawn_timer_asteroid = 0
+            spawn_asteroid()
 
-    # Update sprite group with asteroid positions
-    asteroids_group.empty()
-    for asteroid_rect in asteroid_positions:
-        asteroid_sprite = pygame.sprite.Sprite()
-        asteroid_sprite.rect = asteroid_rect
-        asteroids_group.add(asteroid_sprite)
+        # Move asteroids and update timers
+        for i in range(len(asteroid_positions)):
+            asteroid_positions[i].x += asteroid_speed * asteroid_velocities[i][0]  # Adjust the speed as needed
+            asteroid_positions[i].y += asteroid_speed * asteroid_velocities[i][1]
+            screen.blit(asteroid_image, asteroid_positions[i])
+            asteroid_timers[i] -= clock.get_rawtime()
+            if asteroid_timers[i] <= 0:
+                # Remove asteroid, its timer, and velocity when the time is up
+                asteroid_positions.pop(i)
+                asteroid_velocities.pop(i)
+                asteroid_timers.pop(i)
+                break
 
-    # Check for collision with asteroids
-    if pygame.sprite.spritecollide(player_collision_sprite, asteroids_group, False):
-        # Display game over text
-        game_over_text = pygame.font.Font(None, 72).render("Game Over", True, (255, 0, 0))
-        game_over_rect = game_over_text.get_rect(center=(window_size[0] // 2, window_size[1] // 2))
-        screen.blit(game_over_text, game_over_rect)
+        # Update sprite group with asteroid positions
+        asteroids_group.empty()
+        for asteroid_rect in asteroid_positions:
+            asteroid_sprite = pygame.sprite.Sprite()
+            asteroid_sprite.rect = asteroid_rect
+            asteroids_group.add(asteroid_sprite)
 
-        # Display time survived
-        minutes = total_elapsed_time // 60000
-        seconds = (total_elapsed_time // 1000) % 60
+        # Check for collision with asteroids
+        if pygame.sprite.spritecollide(player_collision_sprite, asteroids_group, False):
+            # Display game over text
+            game_over_text = pygame.font.Font(None, 72).render("Game Over", True, (255, 0, 0))
+            game_over_rect = game_over_text.get_rect(center=(window_size[0] // 2, window_size[1] // 2))
+            screen.blit(game_over_text, game_over_rect)
 
-        time_text = font.render(f"Time Survived: {minutes:02}:{seconds:02}", True, WHITE)
-        time_rect = time_text.get_rect(center=(window_size[0] // 2, window_size[1] // 2 + 50))
-        screen.blit(time_text, time_rect)
+            # Display time survived
+            minutes = total_elapsed_time // 60000
+            seconds = (total_elapsed_time // 1000) % 60
 
+            time_text = font.render(f"Time Survived: {minutes:02}:{seconds:02}", True, WHITE)
+            time_rect = time_text.get_rect(center=(window_size[0] // 2, window_size[1] // 2 + 50))
+            screen.blit(time_text, time_rect)
+
+            pygame.display.flip()
+
+            # Wait for a few seconds before quitting
+            pygame.time.delay(3000)  # 3000 milliseconds (3 seconds)
+            current_state = "start_screen"
+           # sys.exit()
+
+        # Draw player
+        if direction == "right":
+            player = player_right
+        else:
+            player = player_left
+
+        screen.blit(player, player_rect)
+
+        current_time = pygame.time.get_ticks()
+        elapsed_time = current_time - last_time
+        last_time = current_time
+        total_elapsed_time += elapsed_time
+
+        health -= HEALTH_DECREASE_RATE * (elapsed_time / 1000.0)
+
+        # Cap the health value
+        health = max(health, 0.0)
+
+        # Draw health bar background
+        pygame.draw.rect(screen, WHITE, (10, 10, 200, 30))
+
+        # Draw health bar fill
+        pygame.draw.rect(screen, RED, (10, 10, 200 * health, 30))
+
+        # Draw health percentage
+        text = font.render(f"{int(health * 100)}%", True, WHITE)
+
+        screen.blit(text, (220, 15))
+
+        # Update display
         pygame.display.flip()
 
-        # Wait for a few seconds before quitting
-        pygame.time.delay(3000)  # 3000 milliseconds (3 seconds)
-        pygame.quit()
-        sys.exit()
-
-    # Draw player
-    if direction == "right":
-        player = player_right
-    else:
-        player = player_left
-
-    screen.blit(player, player_rect)
-
-    current_time = pygame.time.get_ticks()
-    elapsed_time = current_time - last_time
-    last_time = current_time
-    total_elapsed_time += elapsed_time
-
-    health -= HEALTH_DECREASE_RATE * (elapsed_time / 1000.0)
-
-    # Cap the health value
-    health = max(health, 0.0)
-
-    # Draw health bar background
-    pygame.draw.rect(screen, WHITE, (10, 10, 200, 30))
-
-    # Draw health bar fill
-    pygame.draw.rect(screen, RED, (10, 10, 200 * health, 30))
-
-    # Draw health percentage
-    text = font.render(f"{int(health * 100)}%", True, WHITE)
-
-    screen.blit(text, (220, 15))
-
-    # Update display
-    pygame.display.flip()
-
-    # Set the frame rate
-    clock.tick(60)
+        # Set the frame rate
+        clock.tick(60)
